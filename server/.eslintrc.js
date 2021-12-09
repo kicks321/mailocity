833ef77111d30b20dac78a44b349aa4ba514c562
Jess Graham <jess@jessgraham.me>
2021-12-09 01:54:47 -0500

    First husky commit

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,74 @@
-
-module.exports =  {
-    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-    extends:  [
-      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-      'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-      'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
-    parserOptions:  {
-      ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-      sourceType:  'module',  // Allows for the use of imports
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    '@react-native-community',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react', 'react-hooks', 'jest', 'testing-library', '@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/unbound-method': 'error',
+    'prettier/prettier': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+  overrides: [
+    {
+      // Jest and @typescript-eslint plugin overrides for test files
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      plugins: ['jest', '@typescript-eslint'],
+      rules: {
+        // you should turn the original rule off *only* for test files
+        '@typescript-eslint/unbound-method': 'off',
+        'jest/unbound-method': 'off',
+      },
     },
-    rules:  {
-      '@typescript-eslint/explicit-member-accessibility': 0,
-      '@typescript-eslint/explicit-function-return-type': 0,
-      '@typescript-eslint/no-parameter-properties': 0,
-      '@typescript-eslint/interface-name-prefix': 0
+    {
+      // Enable eslint-plugin-testing-library rules or preset only for matching files
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+      rules: {
+        'testing-library/await-async-query': 'error',
+        'testing-library/no-await-sync-query': 'error',
+        'testing-library/no-debug': 'warn',
+        'testing-library/no-dom-import': 'off',
+        // We don't have access to the DOM in RN
+        'testing-library/prefer-screen-queries': 'off',
+      },
     },
-  };
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'node_modules',
+    'babel.config.js',
+    'metro.config.js',
+    'jest.config.js',
+    'eslintrc.js',
+  ],
+};
--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,52 +1,75 @@
-module.exports =  {
+module.exports = {
+    root: true,
     env: {
-        browser: true,
-        es6: true,
+      es6: true,
+      node: true,
+      jest: true,
     },
     extends: [
-        'eslint:recommended',
-        'prettier/@typescript-eslint',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'plugin:jsx-a11y/strict',
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      '@react-native-community',
+      'plugin:jest/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
     ],
     parser: '@typescript-eslint/parser',
-    extends:  [
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
+      sourceType: 'module',
+      tsconfigRootDir: __dirname,
+      project: ['tsconfig.json'],
     },
+    plugins: ['react', 'react-hooks', 'jest', 'testing-library', '@typescript-eslint'],
     rules: {
-        'react-hooks/exhaustive-deps': 'error',
-        'no-var': 'error',
-        'brace-style': 'error',
-        'prefer-template': 'error',
-        radix: 'error',
-        'space-before-blocks': 'error',
-        'import/prefer-default-export': 'off',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-empty-function': 'off',
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 0,
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/unbound-method': 'error',
+      'prettier/prettier': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': ['error'],
     },
-    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
     overrides: [
-        {
-            files: [
-                '**/*.test.js',
-                '**/*.test.jsx',
-                '**/*.test.tsx',
-                '**/*.spec.js',
-                '**/*.spec.jsx',
-                '**/*.spec.tsx',
-            ],
-            env: {
-                jest: true,
-            },
+      {
+        // Jest and @typescript-eslint plugin overrides for test files
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        plugins: ['jest', '@typescript-eslint'],
+        rules: {
+          // you should turn the original rule off *only* for test files
+          '@typescript-eslint/unbound-method': 'off',
+          'jest/unbound-method': 'off',
         },
-    ],  
-};
+      },
+      {
+        // Enable eslint-plugin-testing-library rules or preset only for matching files
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        extends: ['plugin:testing-library/react'],
+        rules: {
+          'testing-library/await-async-query': 'error',
+          'testing-library/no-await-sync-query': 'error',
+          'testing-library/no-debug': 'warn',
+          'testing-library/no-dom-import': 'off',
+          // We don't have access to the DOM in RN
+          'testing-library/prefer-screen-queries': 'off',
+        },
+      },
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    ignorePatterns: [
+      'node_modules',
+      'babel.config.js',
+      'metro.config.js',
+      'jest.config.js',
+      'eslintrc.js',
+    ],
+  };
+  
