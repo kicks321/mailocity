7ad3cd8ba25cf0d89f467201cf7f9ccef0751da5
rangyia <jess@jessgraham.me>
2022-01-22 01:50:50 -0500

    Merge main -> dev

--- mobile/src/Views/Base/BaseScreen.view.tsx
+++ mobile/src/Views/Base/BaseScreen.view.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { SafeAreaView, ViewStyle } from 'react-native';
+import { ViewStyle } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import BaseStyles from './BaseStyles';
-<<<<<<< HEAD
-import { Background } from '@Components';
-=======
 import { SafeAreaView } from 'react-native-safe-area-context';
->>>>>>> main
 
 interface ScreenViewProps {
     children?: React.ReactNode;
@@ -17,35 +13,13 @@ interface ScreenViewProps {
 
 export const ScreenView: React.FunctionComponent<ScreenViewProps> = (props) => {
     return (
-<<<<<<< HEAD
-        <Animatable.View
-            duration={props.duration || 500}
-            style={[BaseStyles.screenLayoutStyle, BaseStyles.screenColorStyle, props.style]}
-        >
-            {props.image ? (
-                <Background.Image source={require('assets/img/bg-sign-in-4.jpg')}>
-                    <SafeAreaView />
-                    {props.children}
-                    <SafeAreaView />
-                </Background.Image>
-            ) : (
-                <React.Fragment>
-                    <SafeAreaView />
-                    {props.children}
-                    <SafeAreaView />
-                </React.Fragment>
-            )}
-        </Animatable.View>
-=======
         <SafeAreaView>
             <Animatable.View
                 duration={props.duration || 500}
                 style={[BaseStyles.screenLayoutStyle, BaseStyles.screenColorStyle, props.style]}
             >
-                <StatusBar style="auto" />
                 {props.children}
             </Animatable.View>
         </SafeAreaView>
->>>>>>> main
     );
 };
